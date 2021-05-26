659effb8976594e050b4b23ea0e6a3bdc3f4c2c6
brsf11 <brsf11@outlook.com>
2021-05-26 19:58:13 +0800

    add license

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdbool.h>
 #include"packagemerge.h"
 
 int main()
@@ -6,17 +7,25 @@ int main()
     unsigned char maxLength;
     unsigned numCode;
     unsigned histogram[10] = {1,3,2,6,10,1,2,17,9,6};
-    unsigned char codeLength[10];
+    unsigned char codeLength[10]= {0};
 
     int i;
 
-    maxLength = 5;
+    maxLength = 6;
     numCode = 10;
-    packageMerge(maxLength,numCode,histogram,codeLength);
+    int fi = packageMerge(maxLength,numCode,histogram,codeLength);
 
-    for(i=0;i<numCode;i++)
+    if(fi)
     {
-        printf("%2d: %d\n",codeLength[i]);
+        printf("max code length: %d\n",fi);
+        for(i=0;i<numCode;i++)
+        {
+            printf("%2d: %d\n",i,(unsigned)codeLength[i]);
+        }
+    }
+    else
+    {
+        printf("failed\n");
     }
 
     return 0;
