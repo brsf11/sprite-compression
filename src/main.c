c96b9699c30a7e8a8949cf122e365c2d8a5867e6
brsf11 <brsf11@outlook.com>
2021-05-26 19:45:15 +0800

    edit main.c

--- src/main.c
+++ src/main.c
@@ -8,8 +8,16 @@ int main()
     unsigned histogram[10] = {1,3,2,6,10,1,2,17,9,6};
     unsigned char codeLength[10];
 
+    int i;
+
     maxLength = 5;
     numCode = 10;
     packageMerge(maxLength,numCode,histogram,codeLength);
+
+    for(i=0;i<numCode;i++)
+    {
+        printf("%2d: %d\n",codeLength[i]);
+    }
+
     return 0;
 }
