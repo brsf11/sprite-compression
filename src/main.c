7134bdce3d49562d5c252898daef456b5544347a
brsf11 <brsf11@outlook.com>
2021-05-26 19:42:10 +0800

    edit main.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    
+    unsigned char maxLength;
+    unsigned numCode;
+    unsigned histogram[10] = {1,3,2,6,10,1,2,17,9,6};
+    unsigned char codeLength[10];
+
+    maxLength = 5;
+    numCode = 10;
+    packageMerge(maxLength,numCode,histogram,codeLength);
     return 0;
 }
